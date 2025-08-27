66c13258a77ad6ecdba00a1d3052cc983f1805a8
IrlandaAlvarez <mi.alvarezbanda@ugto.mx>
2025-08-27 16:42:34 -0600

    Agregue el programa suma.c

---
+++ Semana4/suma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float n1,n2,suma;
+    printf("Ingresa un número\n");
+    scanf("%f",&n1);
+    printf("Ingresa el segundo número\n");
+    scanf("%f",&n2);
+    suma=n1+n2;
+    printf("Tu suma es de %f",suma);
+
+}
